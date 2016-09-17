8ff36b43574abbe62b5fda536ef4c9de35b1faa6
Kelvin Lu <kelvinlu@mit.edu>
2016-09-17 19:05:18 -0400

    Forreals this time

--- app.js
+++ app.js
@@ -22,17 +22,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({
-                secret: SESSION_SECRET,
-                store: new RedisStore({
-                  url: REDIS_URL
-                }),
-                resave: false,
-                saveUninitialized: true
-                }));
-
-console.log("connected to redis");
-
 app.use('/', routes);
 
 
