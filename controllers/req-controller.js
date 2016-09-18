f77641d651c391ab0935871d69e4efaac1912251
Kelvin Lu <kelvinlu@mit.edu>
2016-09-17 22:37:54 -0400

    Started writing more functions. added routes for seeing and closing requests

--- controllers/req-controller.js
+++ controllers/req-controller.js
@@ -9,10 +9,12 @@ var config = {
 };
 var connection = new Connection(config);
 connection.on('connect', function(err) {
-// If no error, then good to proceed.
     if (err){
+      //Stop and throw an error
       throw err;
     }
+
+    // If no error, then good to proceed.
     console.log("Connected to Azure DB");
 
 });
@@ -27,24 +29,27 @@ RequestController = {};
  * @param name      the name of the student requesting the food
  * @param location  the location of the student requesting food
  * @param food      the food style requested
+ * @return 'DONE' if request input was successful, 'ERR' if not.
  */
 RequestController.requestFood = function(req, res, next){
   //Uhh.. This function just calls another function ._.
   //Going to get rid of this layer - function calling takes time on the computer
+  //foodToSQL();
   var student    = req.query.name,
       location   = req.query.location,
       food       = req.query.food,
-      timeReq    = Date.now();
 
   request = new Request("INSERT SalesLT.Product (Student, Host, Location, Food, TimeReq, TimeServ) OUTPUT INSERTED.ProductID " +
-                      "VALUES (@Name, @Number, @Cost, @Price, CURRENT_TIMESTAMP);", function(err) {
-       if (err) {
-          console.log(err);}
+                      "VALUES (@Name, @Location, @Food, CURRENT_TIMESTAMP);", function(err) {
+        if (err) {
+          console.log(err);
+          res.send("ERR");
+        }
+        res.send("DONE");
       });
-      request.addParameter('Name', TYPES.NVarChar, student);
-      request.addParameter('Number', TYPES.NVarChar , location);
-      request.addParameter('Cost', TYPES.Int, 11);
-      request.addParameter('Price', TYPES.Int,11);
+      request.addParameter('Name', TYPES.NVarChar, Student);
+      request.addParameter('Location', TYPES.NVarChar , location);
+      request.addParameter('Food', TYPES.NVarChar, food);
       request.on('row', function(columns) {
           columns.forEach(function(column) {
             if (column.value === null) {
@@ -57,40 +62,31 @@ RequestController.requestFood = function(req, res, next){
       connection.execSql(request);
 }
 
-RequestController.seeRequests = function(req, res, next){
-
-}
-
-
-module.exports = RequestController;
-
-/*
-
-*/
-function postFoodToSQL(req) {
-
-}
-
-function getFoodFromSQL() {
+RequestController.seeOpenRequests = function(req, res, next){
     request = new Request("SELECT c.CustomerID, c.CompanyName,COUNT(soh.SalesOrderID) AS OrderCount FROM SalesLT.Customer AS c LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID GROUP BY c.CustomerID, c.CompanyName ORDER BY OrderCount DESC;", function(err) {
-    if (err) {
-        console.log(err);}
+      if (err) {
+        console.log(err);
+        res.send("ERR");
+      }
     });
     var result = "";
     request.on('row', function(columns) {
-        columns.forEach(function(column) {
-          if (column.value === null) {
-            console.log('NULL');
-          } else {
-            result+= column.value + " ";
-          }
-        });
-        console.log(result);
-        result ="";
+    columns.forEach(function(column) {
+      if (column.value === null) {
+        console.log('NULL');
+      } else {
+        result+= column.value + " ";
+      }
     });
+    console.log(result);
+    result ="";
+  });
 
-    request.on('done', function(rowCount, more) {
+  request.on('done', function(rowCount, more) {
     console.log(rowCount + ' rows returned');
-    });
-    connection.execSql(request);
+  });
+  connection.execSql(request);
 }
+
+//It's good habit to have this at the very end
+module.exports = RequestController;
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var reqController = require('../controllers/req-controller')
 
 
 router.get('/request/', reqController.requestFood);
+router.get('/see', reqController.seeOpenRequests);
+router.get('/serve', reqController.closeOpenRequest);
 
 module.exports = router;
