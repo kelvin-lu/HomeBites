b64cf7915394cfafbd53de56d05987edfef50db6
Kelvin Lu <kelvinlu@mit.edu>
2016-09-17 22:17:58 -0400

    Fixed missing code

--- controllers/req-controller.js
+++ controllers/req-controller.js
@@ -1,14 +1,9 @@
 var Connection = require('tedious').Connection;
 
 var config = {
-<<<<<<< HEAD
-    userName: '****',
-    password: '****',
-=======
     userName: 'jarvis',
-    password: process.env.hbAzurePW,
->>>>>>> cc1c591901bc804d1259e343c2d52fd00e8a340f
-    server: 'homebites.database.windows.net',
+    password: '',
+    server: 'homeBites.database.windows.net',
     // If you are on Microsoft Azure, you need this:
     options: {encrypt: true, database: 'homeBites'}
 };
@@ -16,13 +11,47 @@ var connection = new Connection(config);
 connection.on('connect', function(err) {
 // If no error, then good to proceed.
     console.log("Connected to Azure DB");
+    executeStatement();
 });
 
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
 
 RequestController = {};
 
+/*
+ * Request a food item for the night. Puts a request in our SQL databases
+ * @param name      the name of the student requesting the food
+ * @param location  the location of the student requesting food
+ * @param food      the food style requested
+ */
 RequestController.requestFood = function(req, res, next){
-    postFoodToSQL(req);
+  //Uhh.. This function just calls another function ._.
+  //Going to get rid of this layer - function calling takes time on the computer
+  var student    = req.query.name,
+      location   = req.query.location,
+      food       = req.query.food,
+      timeReq    = Date.now();
+
+  request = new Request("INSERT SalesLT.Product (Student, Host, Location, Food, TimeReq, TimeServ) OUTPUT INSERTED.ProductID " +
+                      "VALUES (@Name, @Number, @Cost, @Price, CURRENT_TIMESTAMP);", function(err) {
+       if (err) {
+          console.log(err);}
+      });
+      request.addParameter('Name', TYPES.NVarChar, student);
+      request.addParameter('Number', TYPES.NVarChar , location);
+      request.addParameter('Cost', TYPES.Int, 11);
+      request.addParameter('Price', TYPES.Int,11);
+      request.on('row', function(columns) {
+          columns.forEach(function(column) {
+            if (column.value === null) {
+              console.log('NULL');
+            } else {
+              console.log("Product id of inserted item is " + column.value);
+            }
+          });
+      });
+      connection.execSql(request);
 }
 
 RequestController.seeRequests = function(req, res, next){
@@ -32,58 +61,33 @@ RequestController.seeRequests = function(req, res, next){
 
 module.exports = RequestController;
 
-/* 
+/*
 
 */
 function postFoodToSQL(req) {
-    var name = req.query.name;
-    var student = req.query.student;
-    var host = req.query.host;
-    var location = req.query.location;
-    var food = req.query.food;
-    var time = req.query.time;
 
-    request = new Request("INSERT SalesLT.Product (Name, Student, Host, Location, Food, Time) OUTPUT INSERTED.ProductID " +
-                        "VALUES (@Name, @Number, @Cost, @Price, CURRENT_TIMESTAMP);", function(err) {  
-         if (err) {  
-            console.log(err);}  
-        });  
-        request.addParameter('Name', TYPES.NVarChar,'SQL Server Express 2014');  
-        request.addParameter('Number', TYPES.NVarChar , 'SQLEXPRESS2014');  
-        request.addParameter('Cost', TYPES.Int, 11);  
-        request.addParameter('Price', TYPES.Int,11);  
-        request.on('row', function(columns) {  
-            columns.forEach(function(column) {  
-              if (column.value === null) {  
-                console.log('NULL');  
-              } else {  
-                console.log("Product id of inserted item is " + column.value);  
-              }  
-            });  
-        });       
-        connection.execSql(request);
-}  
+}
 
-function getFoodFromSQL() {  
-    request = new Request("SELECT c.CustomerID, c.CompanyName,COUNT(soh.SalesOrderID) AS OrderCount FROM SalesLT.Customer AS c LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID GROUP BY c.CustomerID, c.CompanyName ORDER BY OrderCount DESC;", function(err) {  
-    if (err) {  
-        console.log(err);}  
-    });  
-    var result = "";  
-    request.on('row', function(columns) {  
-        columns.forEach(function(column) {  
-          if (column.value === null) {  
-            console.log('NULL');  
-          } else {  
-            result+= column.value + " ";  
-          }  
-        });  
-        console.log(result);  
-        result ="";  
-    });  
+function getFoodFromSQL() {
+    request = new Request("SELECT c.CustomerID, c.CompanyName,COUNT(soh.SalesOrderID) AS OrderCount FROM SalesLT.Customer AS c LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID GROUP BY c.CustomerID, c.CompanyName ORDER BY OrderCount DESC;", function(err) {
+    if (err) {
+        console.log(err);}
+    });
+    var result = "";
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+          if (column.value === null) {
+            console.log('NULL');
+          } else {
+            result+= column.value + " ";
+          }
+        });
+        console.log(result);
+        result ="";
+    });
 
-    request.on('done', function(rowCount, more) {  
-    console.log(rowCount + ' rows returned');  
-    });  
-    connection.execSql(request);  
-}  
+    request.on('done', function(rowCount, more) {
+    console.log(rowCount + ' rows returned');
+    });
+    connection.execSql(request);
+}
